caab7f00b4e10153e144b6b294fbda67a71f1371
Khunasin Sooksri <86812723+Bearior@users.noreply.github.com>
2025-04-21 04:05:45 +0700

    fix topmenu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import TopMenuItem from './TopMenuItem';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
-import Link from 'next/link'; 
+import Link from 'next/link';
 
 export default async function TopMenu() {
     const session = await getServerSession(authOptions);
@@ -11,7 +11,7 @@ export default async function TopMenu() {
     return (
         <div className={styles.menucontainer}>
             <Link href='/'>
-                <Image 
+                <Image
                     src='/img/logo.png'
                     className={styles.logoimg}
                     alt='logo'
@@ -21,9 +21,10 @@ export default async function TopMenu() {
                 />
             </Link>
 
-            <TopMenuItem title='Booking' pageRef='/booking' />
+            <TopMenuItem title='View Cars' pageRef='/car' />
             <TopMenuItem title='My Booking' pageRef='/mybooking' />
             {/* <TopMenuItem title='Provider' pageRef='/provider' /> */}
+
             {session?.user?.role === 'car-owner' && (
                 <TopMenuItem title='Add Car' pageRef='/add-car' />
             )}
@@ -39,15 +40,28 @@ export default async function TopMenu() {
             {session?.user?.role === 'admin' && (
                 <TopMenuItem title='My Listings' pageRef='/my-listings' />
             )}
+
+
             {session ? (
-                <Link href="/api/auth/signout">
-                    <div className={styles.signedInText}>
-                        Sign-Out of {session.user?.name}
-                        {session.user?.role === 'admin' && (
-                            <span className={styles.adminRole}>(Admin)</span>
-                        )}
-                    </div>
-                </Link>
+                <div className={styles.signedInSection}>
+                    <Link href={"/userProfile"}>
+                        <Image
+                            src="/img/profile-user.png"
+                            alt="profile"
+                            width={32}
+                            height={32}
+                            className={styles.profileIcon}
+                        />
+                    </Link>
+                    <Link href="/api/auth/signout">
+                        <div className={styles.signedInText}>
+                            Sign-Out of {session.user?.name}
+                            {session.user?.role === 'admin' && (
+                                <span className={styles.adminRole}>(Admin)</span>
+                            )}
+                        </div>
+                    </Link>
+                </div>
             ) : (
                 <div className={styles.signinRegister}>
                     <Link href="/api/auth/signin" className={styles.signinLink}>Sign-In</Link>
